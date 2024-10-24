b76b4f37aa41909ff576824a3bc3631a3257726f
Joy Kumar Acharjee <joy29940@gmail.com>
2024-10-24 23:44:09 +0600

    update

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 
 const globalErrHandle = require("./middlewares/globalErrHandle");
 const authRoutes = require("./routes/authRoutes");
@@ -13,22 +11,6 @@ const app = express();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Session
-app.use(
-  session({
-    key: "key",
-    secret: "stress-free-trip",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: false,
-      maxAge: 300000,
-    },
-  })
-);
 
 //Authentication
 app.use("/api/v1/auth", authRoutes);
